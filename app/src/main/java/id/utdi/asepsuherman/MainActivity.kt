bb7a65c97e66409149c3f54b617f03a656cc8b31
Asep <asepsuherman389@gmail.com>
2023-10-20 23:32:52 +0700

    hapus typo

--- app/src/main/java/id/utdi/asepsuherman/MainActivity.kt
+++ app/src/main/java/id/utdi/asepsuherman/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+//import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -21,7 +22,9 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+//import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+//import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 //import androidx.compose.ui.unit.dp
@@ -58,7 +61,7 @@ data class Product(val name: String, val description: String, val imageRes: Int)
 @Composable
 fun ProductListDemo() {
     // Daftar merek produk.
-    val brands = listOf("Adidas", "Nike", "Puma", "Nazsa", "Stoneisland", "Cpcompany", "Lacoste")
+    val brands = listOf("Adidas", "Nike", "Puma", "Nasa", "Stoneisland", "Cpcompany", "Lacoste")
     // selectedBrand digunakan untuk melacak merek yang dipilih oleh pengguna.
     var selectedBrand by remember { mutableStateOf(brands.first()) }
 
@@ -185,35 +188,30 @@ val nikeProducts = listOf(
     Product("Jordan 1 Mid", "This iteration of the AJ1 reimagines Mike's first signature model with a fresh mix of colors. Premium materials, soft cushioning and a padded ankle collar offer total support and celebrate the shoe that started it all.", R.drawable.jordan_1_mid__gs__size_5_5y_wmns_7_white_multi_color_black_554725_190_fast_ship_removebg_preview),
 )
 
-// Daftar produk untuk merek Puma.
 val pumaProducts = listOf(
     Product("PUMA × RIPNDIP", "・PUMA × RIPNDIPのコラボレーション・長年愛されるスウェードスニーカー・RIPNDIPのアイコンであるロードナーマル サイズ 26cm 26.5cm 27cm 27.5cm 28cm 28.5cm 29cm US US 8 US 8.5 US 9 US 9.5 US 10 US 10.5 US 11", R.drawable.ripndip_suede___blk___29_removebg_preview),
     Product("PUMA Suede Classic XXI Men's Sneakers, Black/White, 13", "The Suede Hit The Scene In 1968 And Has Been Changing The Game Ever Since. It’S Been Worn By The Icons Of Every Generation And It’S Stayed Classic Through It All. This Year, We Relaunch The Suede With Fresh Colorways And Subtle Design Updates. Classic As Ever, For All-Time. Features & Benefits Better Materials: The Leather Sourced In This Product Comes From Environmentally Responsible Leather Manufacturing, And Is Audited And Certified Via The Leather Working Group Protocol Details Classic S", R.drawable.puma_suede_classic_xxi_men_s_sneakers__black_white__13_removebg_preview),
     Product("PUMA Roma Basic Little Kids' Shoes", "From the PUMA Archive in 1968, this lightweight training shoe was equipped with comfort-enhancing features such as a thick padded tongue and orthopaedic arch supports. This material update sees the use of leather in homage to the original shoe", R.drawable.roma_basic_little_kids__shoes___puma_removebg_preview),
 )
 
-// Daftar produk untuk merek Nasa.
 val nasaProducts = listOf(
     Product("NASA Casual Coat", "The NASA Casual Coat is a stylish and comfortable jacket for everyday casual wear. With a touch of cool design, this jacket displays the iconic NASA logo, adding an awesome futuristic vibe. Made from high-quality materials, this jacket suits various everyday activities such as strolling, shopping, or meeting friends. It's the perfect choice for those who want to look fashionable while expressing their interest in space exploration.", R.drawable.men_jacket_new_fashion_casual_men_coat_solid_pilot_bomber_jacket___gray___usa_m_removebg_preview),
     Product("NASA Flight Jacket", "The NASA Flight Jacket draws inspiration from classic pilot jackets worn by astronauts. Precision-designed, this jacket offers the perfect blend of style and functionality. Equipped with insulating layers that make it suitable for cold weather, as well as numerous practical pockets for storing essentials. This jacket features a bold NASA logo on the sleeve, exuding a sense of allegiance to this leading space agency. It's the ideal choice for those seeking a cool look and readiness for any adventure.", R.drawable.plus_size_casual_flight_jacket_men___navy_blue___3xl_removebg_preview),
     Product("NASA Japanesee", "The NASA Japanese Embroidered Jacket is the perfect fusion of elegant Japanese style and the spirit of space exploration. This jacket features a design inspired by beautiful Japanese embroidery traditions, with intricate details showcasing the NASA logo, spacecraft imagery, and stars. Crafted from high-quality materials, this jacket offers both comfort and a remarkable appearance. It's the ideal choice for those who appreciate the art of embroidery and have an interest in outer space exploration.", R.drawable.japanese_embroidered_jacket_removebg_preview),
 )
 
-// Daftar produk untuk merek Stone Island.
 val stoneislandProduct = listOf(
     Product("Stone Island Junior Logo-Patch Cotton Hoodie - Neutrals", "logo-patch cotton hoodie from Stone Island Junior featuring logo patch at the sleeve, front pouch pocket, classic hood, long sleeves, ribbed cuffs, ribbed hem, beige and cotton.", R.drawable.stone_island_junior_logo_patch_cotton_hoodie___neutrals_removebg_preview),
     Product("Stone Island Teen Black Cotton Sweatshirt - 14 YEARS", "Discover the new autumn-winter 2022 collection. Luxury fashion for men and children. Wide selection of luxury. Quick checkout. Wide choice of articles. Customer support 24/24. Worldwide shipping. Secure payments", R.drawable.stone_island_teen_black_cotton_sweatshirt___14_years_removebg_preview),
     Product("Stone Island Junior TEEN logo-patch hooded zipper - Neutrals", "TEEN logo-patch hooded zipper from Stone Island Junior featuring cotton, logo patch at the sleeve, front zip fastening, classic hood, long sleeves, straight hem and neutral.", R.drawable.stone_island_junior_teen_logo_patch_hooded_zipper___neutrals_removebg_preview),
 )
 
-// Daftar produk untuk merek CP Company.
 val cpcompanyProduct = listOf(
     Product("The Football Casual", "The Football Casual is often over looked as a trend setter but this cultures influence stretches further than you think. It has helped the brands such as Lacoste and CP Company.", R.drawable.the_football_casual_removebg_preview),
     Product("C.P. Company: The Original Italian Sportswear Brand - Black", "C.P. Company is the Original Italian Sportswear brand specializing in garment dyed technology since 1971. Shop on the Official Online Store.", R.drawable.cp___black_removebg_preview),
     Product("C.P. Company: The Original Italian Sportswear Brand - Blue", "C.P. Company is the Original Italian Sportswear brand specializing in garment dyed technology since 1971. Shop on the Official Online Store.", R.drawable.cp___blue_removebg_preview),
 )
 
-// Daftar produk untuk merek Lacoste.
 val lacosteProduct = listOf(
     Product("Lacoste Men's Classic Fit L.12.12 Polo Shirts Black L1212-031", "Lacoste Men's Classic Fit L.12.12 Polo Shirts Manufacturer #: L1212-031 Color: Black MSRP: $98.00 A signature design from the Lacoste wardrobe, this L.12.12 Polo Shirt in cotton petit piqué combines comfort and elegance. A chic, timeless essential, ideal for any occasion. Ribbed collar and armbands 2-button placket Mother-of-pearl buttons Classic fit Cotton petit piqué Cotton (100%) 100% Authenticity Guaranteed", R.drawable.lacoste_navi_removebg_preview),
     Product("Polo Lacoste Sport Preto", "Polo Masculina Lacoste Sport. Gola em ribana com três botões Piqué tecnológico ultra-dry resistente e respirável Debrum na gola e nos punhos Logo na parte interna da gola Aplique de crocodilo verde bordado no tórax Poliéster (100%)", R.drawable.polo_lacoste_sport_preto_removebg_preview),
